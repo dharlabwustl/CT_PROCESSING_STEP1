bbd6247303c21d78a12722b5a33baafbb1c6a985
dharlabwustl <atulkumar@wustl.edu>
2022-12-20 11:36:19 -0600

    Initial commit 1

---
+++ downloadcodefromgithub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /software/
+git_link=${4}
+git clone ${git_link} #https://github.com/dharlabwustl/EDEMA_MARKERS_PROD.git
+y=${git_link%.git}
+git_dir=$(basename $y)
+mv ${git_dir}/* /software/
+chmod +x /software/*.sh 
+
+SESSION_ID=${1}
+XNAT_USER=${2}
+XNAT_PASS=${3}
+TYPE_OF_PROGRAM=${5}
+
+/software/script_to_call_main_program.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${TYPE_OF_PROGRAM}
