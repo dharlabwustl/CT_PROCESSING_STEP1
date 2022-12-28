b77c10fad7c8263b4eaa0e5fb7d3dad48cb6ada5
dharlabwustl <atulkumar@wustl.edu>
2022-12-28 13:09:59 -0600

    commit day 3

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -1,5 +1,5 @@
 # ./bashtowriteDockerfile.sh
-parent_dir='/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/NWU/PYCHARM/CT_PROCESSING_STEP1'
+parent_dir=${1} #'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/NWU/PYCHARM/CT_PROCESSING_STEP1'
 cat ${parent_dir}/Dockerfile_part1 > ${parent_dir}/Dockerfile
 echo "  "
 command=""
@@ -10,7 +10,7 @@ done
 echo $command
 python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ${parent_dir}/Dockerfile
 # imagename=$1
-imagename='yashengstep1'
+imagename=${2} #'processingforsegm' ##yashengstep1'
 #fsl502py369withpacksnltx
 #docker build -t sharmaatul11/${imagename} .
 #docker push sharmaatul11/${imagename}
