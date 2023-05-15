bf79711afaf39c4c5f8729fef3fc20c185863f85
dharlabwustl <atulkumar@wustl.edu>
2023-05-15 12:31:24 -0500

    commit day 3

--- processing_before_segmentation.sh
+++ processing_before_segmentation.sh
@@ -145,8 +145,8 @@ call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_sn
 outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}" )
 done < <( tail -n +2 "${niftifile_csvfilename}" )
 ################################################
-echo "outputfiles_present::${outputfiles_present}::outputfiles_present"
-echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
+echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+#echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
 if [[ $outputfiles_present -eq 1 ]] ; then
   echo " I AM THE ONE"
 fi
