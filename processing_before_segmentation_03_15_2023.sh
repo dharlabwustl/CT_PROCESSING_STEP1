477ff5abcd7831b6702b10a499b184d6d12d5317
dharlabwustl <atulkumar@wustl.edu>
2024-03-18 14:08:46 -0500

    commit day 3

--- processing_before_segmentation_03_15_2023.sh
+++ processing_before_segmentation_03_15_2023.sh
@@ -144,7 +144,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
     echo " I AM THE ONE"
   fi
   if [[ "${outputfiles_present: -1}" -eq 1 ]]; then
-
+    echo " I AM THE SECOND"
     echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
     copy_scan_data ${niftifile_csvfilename} ${working_dir}
     working_dir=/workinginput
