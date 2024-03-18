aa38017084c08a6658c4b18e4828a491b40b97ba
dharlabwustl <atulkumar@wustl.edu>
2024-03-18 14:13:23 -0500

    commit day 3

--- processing_before_segmentation_03_15_2023.sh
+++ processing_before_segmentation_03_15_2023.sh
@@ -178,19 +178,19 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
     echo " FILES NOT PRESENT I AM WORKING ON IT"
     ##############################################################
 
-    this_csvfilename='/workinginput/all_files_present_flag_df.csv'
-    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _normalized.nii.gz _levelset.nii.gz _levelset_bet.nii.gz _4DL_seg.nii.gz)
-    outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
-    csvfile_for_redcap='/workinginput/csvfile_for_redcap_preprocessing.csv'
-    csvfile_scan_selection_for_redcap_call=('csvfile_preprocessing_for_redcap' ${this_csvfilename} ${csvfile_for_redcap}) # 'Name' scan_name)
-    outputfiles_present=$(python3 fillmaster_session_list.py "${csvfile_scan_selection_for_redcap_call[@]}")
-
-    this_session_id=${sessionID}
-    xml_filename=${working_dir}/${this_session_id}.xml
-    filename_xml=$(basename ${xml_filename})   #args.stuff[2]
-    dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
-    download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
-    outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
+#    this_csvfilename='/workinginput/all_files_present_flag_df.csv'
+#    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _normalized.nii.gz _levelset.nii.gz _levelset_bet.nii.gz _4DL_seg.nii.gz)
+#    outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
+#    csvfile_for_redcap='/workinginput/csvfile_for_redcap_preprocessing.csv'
+#    csvfile_scan_selection_for_redcap_call=('csvfile_preprocessing_for_redcap' ${this_csvfilename} ${csvfile_for_redcap}) # 'Name' scan_name)
+#    outputfiles_present=$(python3 fillmaster_session_list.py "${csvfile_scan_selection_for_redcap_call[@]}")
+#
+#    this_session_id=${sessionID}
+#    xml_filename=${working_dir}/${this_session_id}.xml
+#    filename_xml=$(basename ${xml_filename})   #args.stuff[2]
+#    dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
+#    download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
+#    outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
 
     fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap}) #${subj_listfile})
     outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
