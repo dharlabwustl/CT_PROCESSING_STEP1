55292b8a708f4f5c7b08e80cfd399642312e4206
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-08 14:32:56 -0400

    sdklfjdk

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -4,19 +4,19 @@ XNAT_USER=${2}
 XNAT_PASS=${3}
 XNAT_HOST=${4}
 TYPE_OF_PROGRAM=${5}
-input=$XNAT_HOST ##"one::two::three::four"
-# Check if '::' is present
-if echo "$input" | grep -q "+"; then
-  # Set the delimiter
-  IFS='+'
+# input=$XNAT_HOST ##"one::two::three::four"
+# # Check if '::' is present
+# if echo "$input" | grep -q "+"; then
+#   # Set the delimiter
+#   IFS='+'
 
-  # Read the split words into an array
-  read -ra ADDR <<< "$input"
-  export XNAT_HOST=${ADDR[0]} 
-else
-export XNAT_HOST=${XNAT_HOST} 
-    echo "'+' is not present in the string"
-fi
+#   # Read the split words into an array
+#   read -ra ADDR <<< "$input"
+#   export XNAT_HOST=${ADDR[0]} 
+# else
+# export XNAT_HOST=${XNAT_HOST} 
+#     echo "'+' is not present in the string"
+# fi
 
 echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
 if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
@@ -50,9 +50,9 @@ fi
 
 if [[ ${TYPE_OF_PROGRAM} == 'PROJECT_LEVEL_PRE_PROCESSING' ]] ;
 then
-    /software/project_level_pre_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS "${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}" ##$XNAT_HOST ##/input /output
+    /software/project_level_pre_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST ##"${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}" ##$XNAT_HOST ##/input /output
 fi
 if [[ ${TYPE_OF_PROGRAM} == 'PROJECT_LEVEL_AFTER_PROCESSING' ]] ;
 then
-    /software/project_level_after_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS "${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}"  #### $XNAT_HOST ##/input /output
+    /software/project_level_after_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST  ###"${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}"  #### $XNAT_HOST ##/input /output
 fi
