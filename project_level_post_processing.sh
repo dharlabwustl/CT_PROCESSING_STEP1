d602fc320900a80c2064aa68a64ec4034e11a525
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-08 16:11:42 -0400

    adsfadsfds

--- project_level_post_processing.sh
+++ project_level_post_processing.sh
@@ -3,6 +3,8 @@ export XNAT_USER=${2}
 export XNAT_PASS=${3}
 export XNAT_HOST=${4}
 project_ID=${1}
+# counter_start=${5}
+# counter_end=${6}
 working_dir=/workinginput
 output_directory=/workingoutput
 final_output_directory=/outputinsidedocker
@@ -13,6 +15,7 @@ DICOMFILEDIR=/DICOMFILEDIR
 working=/working
 input=/input
 output=/output
+software=/software
 function directory_to_create_destroy(){
 
 rm  -r    ${working_dir}/*
@@ -23,7 +26,7 @@ rm  -r    ${final_output_directory}/*
 rm  -r    ${ZIPFILEDIR}/*
 rm  -r    ${NIFTIFILEDIR}/*
 rm  -r    ${DICOMFILEDIR}/*
-# rm  -r    ${working}/*
+rm  -r    ${working}/*
 rm  -r    ${input}/*
 rm  -r    ${output}/*
 
@@ -37,23 +40,24 @@ rm  -r    ${output}/*
 # /callfromgithub/downloadcodefromgithub.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${git_repo} ${script_number}  ${snipr_host}  EC6A2206FF8C1D87D4035E61C99290FF
 # }
 directory_to_create_destroy
-sessions_list=${working}/session.csv 
+sessions_list=${software}/session.csv 
 curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/projects/${project_ID}/experiments/?format=csv > ${sessions_list}
 ######################################
 count=0
   while IFS=',' read -ra array; do
+    # if [ ${count} -ge ${counter_start} ]; then
     echo SESSION_ID::${array[0]}
     SESSION_ID=${array[0]}  #SNIPR02_E10218 ##SNIPR02_E10112 #
     SESSION_NAME=${array[5]} 
 
     # echo SESSION_NAME::${SESSION_NAME}
     directory_to_create_destroy
-    /software/processing_after_segmentation.sh ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST 
-
+    /software/processing_after_segmentation.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST 
+    # fi
     # echo "$SESSION_ID,$SESSION_NAME" >> ${list_accomplished}
     count=$((count+1))
 #     fi
-    if [ ${count} -gt 0 ]; then
-    break
-    fi
+    # if [ ${count} -ge 3 ]; then
+    # break
+    # fi
 done < <(tail -n +2 "${sessions_list}")
