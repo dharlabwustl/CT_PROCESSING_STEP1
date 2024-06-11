aca817f1adacf0dfdc01cdeb0a83a3b0c509abde
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-11 13:15:00 -0500

    hakjdhfkjdh

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -4,19 +4,22 @@ XNAT_USER=${2}
 XNAT_PASS=${3}
 XNAT_HOST=${4}
 TYPE_OF_PROGRAM=${5}
-# input=$XNAT_HOST ##"one::two::three::four"
-# # Check if '::' is present
-# if echo "$input" | grep -q "+"; then
-#   # Set the delimiter
-#   IFS='+'
+input=$XNAT_HOST ##"one::two::three::four"
+# Check if '::' is present
+input=$XNAT_HOST ##"one::two::three::four"
+# Check if '::' is present
+if echo "$input" | grep -q "+"; then
+  # Set the delimiter
+  IFS='+'
 
-#   # Read the split words into an array
-#   read -ra ADDR <<< "$input"
-#   export XNAT_HOST=${ADDR[0]} 
-# else
-# export XNAT_HOST=${XNAT_HOST} 
-#     echo "'+' is not present in the string"
-# fi
+  # Read the split words into an array
+  read -ra ADDR <<< "$input"
+  export XNAT_HOST=${ADDR[0]} 
+  SUBTYPE_OF_PROGRAM=${ADDR[1]} 
+else
+export XNAT_HOST=${XNAT_HOST} 
+    echo "'+' is not present in the string"
+fi
 
 echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
 if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
@@ -47,12 +50,15 @@ then
 fi
 
 ##################
+if [[ ${TYPE_OF_PROGRAM} == 'PROJECT_LEVEL' ]]; then
+echo "I AM HERE"
 
-if [[ ${TYPE_OF_PROGRAM} == 'PROJECT_LEVEL_PRE_PROCESSING' ]] ;
+if [[ ${SUBTYPE_OF_PROGRAM} == 'PROJECT_LEVEL_PRE_PROCESSING' ]] ;
 then
-    /software/project_level_pre_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST ##"${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}" ##$XNAT_HOST ##/input /output
+    /software/project_level_pre_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS "${ADDR[0]}"  "${ADDR[2]}" "${ADDR[3]}" /input /output
 fi
-if [[ ${TYPE_OF_PROGRAM} == 'PROJECT_LEVEL_AFTER_PROCESSING' ]] ;
+if [[ ${SUBTYPE_OF_PROGRAM} == 'PROJECT_LEVEL_AFTER_PROCESSING' ]] ;
 then
-    /software/project_level_after_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST  ###"${ADDR[0]}" "${ADDR[1]}" "${ADDR[2]}"  #### $XNAT_HOST ##/input /output
+    /software/project_level_after_processing.sh $SESSION_ID $XNAT_USER $XNAT_PASS "${ADDR[0]}"  "${ADDR[2]}" "${ADDR[3]}" /input /output
+fi
 fi
