f30a05fa289f26bf0508f4331d30b6b01b54fe7c
dharlabwustl <atulkumar@wustl.edu>
2026-01-15 22:25:06 -0600

    commit day 3

--- processing_after_segmentation_03_21_2024.sh
+++ processing_after_segmentation_03_21_2024.sh
@@ -224,8 +224,9 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
         download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
         outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
 
-        fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap}) #${subj_listfile})
-        outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
+#        fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap}) #${subj_listfile})
+#        outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
+        /software/write_to_redcap.sh ${sessionID} ${csvfile_for_redcap}
     ##############
 
   else
